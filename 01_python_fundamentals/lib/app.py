d5573c7bc94a331b95ee1b87e95c6d934b349544
Louis <Louis.Medina@flatironschool.com>
2023-03-13 15:17:10 -0400

    complete all remaining deliverables

--- 01_python_fundamentals/lib/app.py
+++ 01_python_fundamentals/lib/app.py
@@ -23,8 +23,8 @@ import ipdb
 
     # Note => Feel free to set your own values for "pet_mood" to view various outputs.
 
-pet_mood = "XYZ!"
-pet_name = "Rose"
+# pet_mood = "XYZ!"
+# pet_name = "Rose"
 
 # ipdb.set_trace()
 
@@ -80,23 +80,57 @@ def say_hello(name = "Student"):
     # pet_greeting("Rose") => "Rose says hello!"
     # pet_greeting("Spot") => "Spot says hello!"
 
+# Ternary Operators Can Be Used To Set Default Values
+# def check_mood():
+#     print("Rose needs to be fed.") if (pet_mood == "Hungry!") else (print("Rose is all good.")
+
 # Declaration
 def pet_greeting(name = "Pet"):
     print(f"{name} says hello!")
 
 # Invokation
-pet_greeting()
-pet_greeting("Rose")
-pet_greeting("Spot")
+# pet_greeting()
+# pet_greeting("Rose")
+# pet_greeting("Spot")
 
 # 5. ✅ Move conditional logic from Deliverable 1 into a function (pet_status) so that we may use it with different pets / moods
     # Test invocation of "pet_status" in ipdb using "pet_status(pet_name, pet_mood)"
     # pet_status("Rose", "Hungry!") => "Rose needs to be fed."
-    # pet_greeting("Spot", "Rowdy!") => "Spot needs a walk."
-    # pet_greeting("Bud", "Relaxed") => "Bud is all good."
+    # pet_status("Spot", "Rowdy!") => "Spot needs a walk."
+    # pet_status("Bud", "Relaxed") => "Bud is all good."
     
     # Take a moment to note that "pet_name" and "pet_mood" parameters are within Local Scope and take priority over "pet_name" and "pet_mood"
     # in Global Scope.
+
+# Global Scope
+# pet_mood = "Rowdy!"
+# pet_name = "Rose"
+
+# test_var = "Test"
+
+# def pet_status(my_pet_name, my_pet_mood):
+    
+#     global test_var
+#     test_var = "New Value"
+
+#     # print(test_var)
+
+#     # Function (Local) Scope
+#     if my_pet_mood == "Hungry!":
+#         print(f"{my_pet_name} needs to be fed!")
+#     elif my_pet_mood == "Rowdy!":
+#         print(f"{my_pet_name} needs to be walked!")
+#     else:
+#         print(f"{my_pet_name} is all good!")
+
+# Limitations of Function (Local) Scope
+# NameError: name 'test_var' is not defined
+# print(test_var)
+
+# pet_status(pet_name, my_pet_mood)
+# pet_status("Spot", "Rowdy!")
+# pet_status("Bud", "Relaxed")
+
 
 # 6. ✅ Create a function (pet_birthday) that will increment a pet's age up by 1. Use try / except to handle errors. 
     # If our function is given an incorrect datatype, it should handle the TypeError exception and alert the user
@@ -105,7 +139,23 @@ pet_greeting("Spot")
 
     # Note => To view more common Python exceptions, visit https://docs.python.org/3/library/exceptions.html
 
+def pet_birthday(age = 0.1):
+    
+    age = "This is in Global Scope"
+    
+    try:
+        age = age + 1
+        print(f"Happy Birthday! Your pet is now {age}.")
+
+    # Only in the case of TypeError firing do we print the String below.
+    except TypeError:
+        print("Type Error Occurred!")
+
+    # except NameError:
+    #     print("Type Error Occurred!")
+
+pet_birthday(10)
+pet_birthday("oops")
+
 # 🚨 To create an ipdb breakpoint, comment / uncomment line below:
-# ipdb.set_trace()
-
-
+ipdb.set_trace()
