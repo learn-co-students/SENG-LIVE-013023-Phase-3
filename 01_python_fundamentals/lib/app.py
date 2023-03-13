564c6ceab4ffc37497e1e6036a07f932ecc2523c
Louis <Louis.Medina@flatironschool.com>
2023-03-13 14:36:12 -0400

    Complete deliverables 2 - 4

--- 01_python_fundamentals/lib/app.py
+++ 01_python_fundamentals/lib/app.py
@@ -23,7 +23,7 @@ import ipdb
 
     # Note => Feel free to set your own values for "pet_mood" to view various outputs.
 
-pet_mood = "Rowdy!"
+pet_mood = "XYZ!"
 pet_name = "Rose"
 
 # ipdb.set_trace()
@@ -32,32 +32,62 @@ pet_name = "Rose"
 # Python => elif
 
 # if (pet_mood == "Hungry!"){
-if pet_mood == "Hungry!":
-    print("Rose needs to be fed!")
-elif pet_mood == "Rowdy!":
-    print("Rose needs to be walked!")
-# elif pet_mood == "Feisty!":
+# if pet_mood == "Hungry!":
+#     print("Rose needs to be fed!")
+# elif pet_mood == "Rowdy!":
 #     print("Rose needs to be walked!")
-# elif pet_mood == "Mischievous!":
-#     print("Rose needs to be walked!")
-# elif pet_mood == "Bored!":
-#     print("Rose needs to be walked!")
-else:
-    print("Rose is all good!")
+# # elif pet_mood == "Feisty!":
+# #     print("Rose needs to be walked!")
+# # elif pet_mood == "Mischievous!":
+# #     print("Rose needs to be walked!")
+# # elif pet_mood == "Bored!":
+# #     print("Rose needs to be walked!")
+# else:
+#     print("Rose is all good!")
 
+# JavaScript => CONDITION ? IF TRUE : IF FALSE
+# Python => IF TRUE if CONDITION else IF FALSE
 
 # 2. ✅ Create a ternary operator using "pet_mood" as a condition:
-    # If pet_food is "Hungry!" => "Rose needs to be fed."
+    # If pet_mood is "Hungry!" => "Rose needs to be fed."
     # In all other cases => "Rose is all good."
+
+# print("Rose needs to be fed.") if (pet_mood == "Hungry!") else (print("Rose is all good."))
+
+# JavaScript => function / const + Arrow Syntax
+# Python => def
 
 # 3. ✅ Create a function (say_hello) that returns the string "Hello, world!"
     # Test invocation of "say_hello" in ipdb using "say_hello()"
     # say_hello() => "Hello, world!"
 
+# Declaration
+def say_hello(name = "Student"):
+    print(f"Hello, {name}!")
+
+# Invocation
+
+# TypeError: say_hello() missing 1 required positional argument: 'name'
+    # Cannot Invoke Functions Containing Params in Python Without Passing Args 
+    # UNLESS Default Values Are Set
+# say_hello()
+# say_hello("Louis")
+# say_hello("Sally")
+# say_hello("John")
+
 # 4. ✅ Create a function (pet_greeting) that will return a string with interpolated pet's name
     # Test invocation of "pet_greeting" in ipdb using "pet_greeting()"
     # pet_greeting("Rose") => "Rose says hello!"
     # pet_greeting("Spot") => "Spot says hello!"
+
+# Declaration
+def pet_greeting(name = "Pet"):
+    print(f"{name} says hello!")
+
+# Invokation
+pet_greeting()
+pet_greeting("Rose")
+pet_greeting("Spot")
 
 # 5. ✅ Move conditional logic from Deliverable 1 into a function (pet_status) so that we may use it with different pets / moods
     # Test invocation of "pet_status" in ipdb using "pet_status(pet_name, pet_mood)"
